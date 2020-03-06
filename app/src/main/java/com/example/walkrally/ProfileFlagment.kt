7b4e587f8c4146adf53e085727e7e436a53099f9
thanaphon0737 <35342731+thanaphon0737@users.noreply.github.com>
2020-03-06 21:53:36 +0700

    update logout
    create button logout in Profile fragment

--- app/src/main/java/com/example/walkrally/ProfileFlagment.kt
+++ app/src/main/java/com/example/walkrally/ProfileFlagment.kt
@@ -1,5 +1,6 @@
 package com.example.walkrally
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -8,6 +9,9 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -34,6 +38,8 @@ class ProfileFlagment : Fragment() {
         val  btnLogout = view.findViewById<Button>(R.id.logoutButton)
         btnLogout.setOnClickListener{view ->
             Log.d("testbut","Good")
+            FirebaseAuth.getInstance().signOut()
+            startActivity(Intent(activity,LoginMain::class.java))
         }
         return view
     }
