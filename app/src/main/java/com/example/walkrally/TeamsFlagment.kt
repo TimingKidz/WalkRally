c808fabb090eeddd7f7e26ce9ee5db55c968afd7
SIRIN RATTANARUANG-AMPHAI <sirin_r@cmu.ac.th>
2020-04-16 16:30:05 +0700

    ...

--- app/src/main/java/com/example/walkrally/TeamsFlagment.kt
+++ app/src/main/java/com/example/walkrally/TeamsFlagment.kt
@@ -5,6 +5,8 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ListView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
@@ -14,24 +16,26 @@ import java.lang.reflect.Member
 
 class TeamsFlagment : Fragment() {
  //   var t_list = ArrayList<Team>()
-    lateinit var member: Member
+   // lateinit var teamname: TextView
+    lateinit var teammem: ListView
 
-    lateinit var ref: DatabaseReference
-           val Current_user = ref.child(FirebaseAuth.getInstance().currentUser!!.uid).key
-    var team_id = FirebaseDatabase.getInstance().getReference().child("Users").child(Current_user!!).child("team")
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        readdata()
-    }
+ //   lateinit var ref: DatabaseReference
+ //          val Current_user = ref.child(FirebaseAuth.getInstance().currentUser!!.uid).key
+ //   var team_id = FirebaseDatabase.getInstance().getReference().child("Users").child(Current_user!!).child("team")
+  //  override fun onCreate(savedInstanceState: Bundle?) {
+  //      super.onCreate(savedInstanceState)
+   //     readdata()
+   // }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.custom_team_member, container, false)
-        member = view.findViewById(R.id.member)
-   //     readData()
+        val view = inflater.inflate(R.layout.fragment_teams_flagment, container, false)
+    //    teamname = view.findViewById(R.id.teamname)
+        teammem = view.findViewById(R.id.teammem)
+        readdata()
         return view
     }
 
@@ -46,18 +50,18 @@ class TeamsFlagment : Fragment() {
             }
 
             override fun onChildChanged(p0: DataSnapshot, p1: String?) {
-                val id = p0.key.toString()
-                val c = p0.childrenCount.toString()
-                var user = p0.child("team_id").value.toString()
-                Log.d("tt", user)
+      //          val id = p0.key.toString()
+      //          val c = p0.childrenCount.toString()
+    //            var user = p0.child("team_id").value.toString()
+     //           Log.d("tt", user)
 //                listView.adapter = TeamAdapter(applicationContext,R.layout.custom_team_list,t_list)
             }
 
             override fun onChildAdded(p0: DataSnapshot, p1: String?) {
-                val id = p0.key.toString()
-                val c = p0.childrenCount.toString()
-                var user = p0.child("team_id").value.toString()
-                Log.d("tt", user)
+    //            val id = p0.key.toString()
+      //          val c = p0.childrenCount.toString()
+     //           var user = p0.child("team_id").value.toString()
+     //           Log.d("tt", user)
 //                FirebaseDatabase.getInstance().getReference().child(team_Path).child(id).child("mcount").setValue(c)
 //                listView.adapter = TeamAdapter(applicationContext,R.layout.custom_team_list,t_list)
             }
@@ -69,9 +73,9 @@ class TeamsFlagment : Fragment() {
         }
 //        val team_name_list = FirebaseDatabase.getInstance().getReference(team_Path).orderByChild("id")
 //        team_name_list.addChildEventListener(postListener)
-        val team_list = FirebaseDatabase.getInstance().getReference("TeamMembers").orderByKey()
-        Log.d("team_key",team_list.toString())
-        team_list.addChildEventListener(Listener)
+     //   val team_list = FirebaseDatabase.getInstance().getReference("TeamMembers").orderByKey()
+     //   Log.d("team_key",team_list.toString())
+    //    team_list.addChildEventListener(Listener)
 
 
     }
